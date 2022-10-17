ea04417499fa57d0e483b3920c9677c2fc043778
prtvi <prithvippv25@gmail.com>
2022-10-17 16:05:35 +0530

    finish algo visualization
    translate algo and visualization complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,164 +3,166 @@ import React from 'react';
 import Grid from './components/Grid';
 import InputForm from './components/InputForm';
 
-class Node {
-	constructor(x, y, state, totalRows) {
-		this.x = x;
-		this.y = y;
-		this.state = state;
-		this.neighbours = [];
-		this.totalRows = totalRows;
-	}
-
-	isBarrier() {
-		return this.state === 1;
-	}
-
-	makeBarrier() {
-		this.state = 1;
-	}
-
-	isReset() {
-		return this.state === 0;
-	}
-
-	reset() {
-		this.state = 0;
-	}
-
-	makeClosed() {
-		this.state = 2;
-	}
-
-	makeOpen() {
-		this.state = 3;
-	}
-
-	makeStart() {
-		this.state = 5;
-	}
-
-	makeEnd() {
-		this.state = 9;
-	}
-
-	makePath() {
-		this.state = 4;
-	}
-
-	// pending
-	updateNeighbours(grid) {
-		this.neighbours = [];
-
-		// BOTTOM neighbour
-		if (this.y < this.totalRows - 1 && !grid[this.y + 1][this.x].isBarrier())
-			this.neighbours.push(grid[this.y + 1][this.x]);
-
-		// TOP neighbour
-		if (this.y > 0 && !grid[this.y - 1][this.x].isBarrier())
-			this.neighbours.push(grid[this.y - 1][this.x]);
-
-		// RIGHT neighbour
-		if (this.x < this.totalRows - 1 && !grid[this.y][this.x + 1].isBarrier())
-			this.neighbours.push(grid[this.y][this.x + 1]);
-
-		// LEFT neighbour
-		if (this.x > 0 && !grid[this.y][this.x - 1].isBarrier())
-			this.neighbours.push(grid[this.y][this.x - 1]);
-	}
-}
+import { PriorityQueue } from './utils/pq';
+import {
+	getGrid,
+	getGridStates,
+	cellToCoords,
+	coordsToCell,
+} from './utils/cell';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-const getNodesGrid = function (gridSize) {
-	const gridNodes = [];
-	for (let i = 0; i < gridSize; i++) {
-		const row = [];
-
-		for (let j = 0; j < gridSize; j++) row.push(new Node(j, i, 0, gridSize));
-
-		gridNodes.push(row);
-	}
-
-	return gridNodes;
-};
-
-const getNodeStates = function (nodesGrid) {
-	return nodesGrid.map(row => row.map(node => node.state));
-};
 
 export default function App() {
 	const [speed, setSpeed] = React.useState(50);
 
-	const [gridSize, setGridSize] = React.useState(7);
-	const [gridNodes, updateGridNodes] = React.useState(getNodesGrid(gridSize));
+	const [gridSize, setGridSize] = React.useState(10);
+	const [grid, updateGrid] = React.useState(getGrid(gridSize));
 
-	const [startNode, setStartNode] = React.useState(null);
-	const [endNode, setEndNode] = React.useState(null);
+	const [startCell, setStart] = React.useState(null);
+	const [endCell, setEnd] = React.useState(null);
 
-	const [nodesClicked, updateNodesClicked] = React.useState(0);
+	const [cellsClicked, updateCellsClicked] = React.useState(0);
 
 	const updateGridOnInput = function (newGridSize) {
-		setStartNode(null);
-		setEndNode(null);
-		updateNodesClicked(0);
-		updateGridNodes(curr => getNodesGrid(newGridSize));
+		setStart(null);
+		setEnd(null);
+		updateCellsClicked(0);
+		updateGrid(curr => getGrid(newGridSize));
 	};
 
 	const updateGridOnClick = function (x, y) {
-		const node = gridNodes[y][x];
+		const cell = grid[y][x];
 
-		if (nodesClicked >= 2) {
+		if (cellsClicked === 0 && !startCell && !endCell) {
+			// add start cell
+			updateCellsClicked(cellsClicked + 1);
+			updateGrid(grid => {
+				if (cell.isReset()) {
+					cell.makeStart();
+					setStart(cell);
+				}
+
+				return grid;
+			});
+		} else if (cellsClicked === 1 && !endCell && startCell) {
+			// add end cell
+			updateCellsClicked(cellsClicked + 1);
+			updateGrid(grid => {
+				if (cell.isReset()) {
+					cell.makeEnd();
+					setEnd(cell);
+				}
+
+				return grid;
+			});
+		} else if (cellsClicked >= 2 && startCell && endCell) {
 			// add/remove barrier
-
-			updateNodesClicked(nodesClicked + 1);
-			updateGridNodes(grid => {
-				if (node.isBarrier()) node.reset();
-				else if (node.isReset()) node.makeBarrier();
-
-				return grid;
-			});
-		} else if (nodesClicked === 1) {
-			// add end node
-
-			updateGridNodes(grid => {
-				if (node.isReset()) {
-					node.makeEnd();
-					updateNodesClicked(nodesClicked + 1);
-
-					setEndNode(node);
-				}
-
-				return grid;
-			});
-		} else if (nodesClicked === 0) {
-			// add start node
-
-			updateGridNodes(grid => {
-				if (node.isReset()) {
-					node.makeStart();
-					updateNodesClicked(nodesClicked + 1);
-
-					setStartNode(node);
-				}
+			updateCellsClicked(cellsClicked + 1);
+			updateGrid(grid => {
+				if (cell.isReset()) cell.makeBarrier();
+				else if (cell.isBarrier()) cell.reset();
 
 				return grid;
 			});
 		}
 	};
 
-	const startVis = async function () {
-		console.log('start vis');
-		await delay(speed);
+	const h = function (c1, c2) {
+		const [x1, y1] = [c1.x, c1.y];
+		const [x2, y2] = [c2.x, c2.y];
 
-		console.log(startNode, endNode);
-		console.log(gridNodes);
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+	};
+
+	const reconstructPath = function (cameFrom, currentCoords, grid) {
+		while (currentCoords in cameFrom) {
+			currentCoords = cameFrom[currentCoords];
+			coordsToCell(grid, currentCoords).makePath();
+		}
+	};
+
+	const algorithm = async function (grid, start, end) {
+		const startCoords = cellToCoords(start);
+		const endCoords = cellToCoords(end);
+
+		let count = 0;
+		const openSet = new PriorityQueue();
+		openSet.enqueue([count, startCoords], 0);
+		const cameFrom = {};
+
+		const gScore = {};
+		grid.forEach(row =>
+			row.forEach(cell => (gScore[cellToCoords(cell)] = Infinity))
+		);
+		gScore[startCoords] = 0;
+
+		const fScore = {};
+		grid.forEach(row =>
+			row.forEach(cell => (fScore[cellToCoords(cell)] = Infinity))
+		);
+		fScore[startCoords] = h(start, end);
+
+		const openSetHash = new Set([startCoords]);
+
+		while (!openSet.isEmpty()) {
+			const currentCellCoords = openSet.dequeue().element[1];
+			const currentCell = coordsToCell(grid, currentCellCoords);
+
+			openSetHash.delete(currentCellCoords);
+
+			if (currentCellCoords === endCoords) {
+				reconstructPath(cameFrom, endCoords, grid);
+
+				end.makeEnd();
+				start.makeStart();
+				return true;
+			}
+
+			// eslint-disable-next-line
+			currentCell.neighbours.forEach(neighbour => {
+				const neighbourCoords = cellToCoords(neighbour);
+				const tempGScore = gScore[currentCellCoords] + 1;
+
+				if (tempGScore < gScore[neighbourCoords]) {
+					cameFrom[neighbourCoords] = currentCellCoords;
+					gScore[neighbourCoords] = tempGScore;
+					fScore[neighbourCoords] = tempGScore + h(neighbour, end);
+
+					if (!openSetHash.has(neighbourCoords)) {
+						count += 1;
+
+						openSet.enqueue([count, neighbourCoords], fScore[neighbourCoords]);
+						openSetHash.add(neighbourCoords);
+
+						neighbour.makeOpen();
+					}
+				}
+			});
+
+			await delay(speed);
+
+			updateGrid(grid => grid.slice());
+
+			if (currentCell !== start) currentCell.makeClosed();
+		}
+
+		return false;
+	};
+
+	const startVis = async function () {
+		if (startCell && endCell) {
+			// update neighbours
+			grid.forEach(row => row.forEach(cell => cell.updateNeighbours(grid)));
+
+			await algorithm(grid, startCell, endCell);
+		}
 	};
 
 	return (
 		<div className="main">
 			<header>
-				<h1>A</h1>
+				<h1>A* shortest path algorithm visualization</h1>
 			</header>
 			<InputForm
 				updateGridOnInput={updateGridOnInput}
@@ -168,10 +170,7 @@ export default function App() {
 				setSpeed={setSpeed}
 				startVis={startVis}
 			/>
-			<Grid
-				grid={getNodeStates(gridNodes)}
-				updateGridOnClick={updateGridOnClick}
-			/>
+			<Grid grid={getGridStates(grid)} updateGridOnClick={updateGridOnClick} />
 		</div>
 	);
 }
---
+++ src/utils/cell.js
@@ -0,0 +1,91 @@
+class Cell {
+	constructor(x, y, state, totalRows) {
+		this.x = x;
+		this.y = y;
+		this.state = state;
+		this.neighbours = [];
+		this.totalRows = totalRows;
+	}
+
+	isBarrier() {
+		return this.state === 1;
+	}
+
+	makeBarrier() {
+		this.state = 1;
+	}
+
+	isReset() {
+		return this.state === 0;
+	}
+
+	reset() {
+		this.state = 0;
+	}
+
+	makeClosed() {
+		this.state = 2;
+	}
+
+	makeOpen() {
+		this.state = 3;
+	}
+
+	makeStart() {
+		this.state = 5;
+	}
+
+	makeEnd() {
+		this.state = 9;
+	}
+
+	makePath() {
+		this.state = 4;
+	}
+
+	updateNeighbours(grid) {
+		this.neighbours = [];
+
+		// BOTTOM neighbour
+		if (this.y < this.totalRows - 1 && !grid[this.y + 1][this.x].isBarrier())
+			this.neighbours.push(grid[this.y + 1][this.x]);
+
+		// TOP neighbour
+		if (this.y > 0 && !grid[this.y - 1][this.x].isBarrier())
+			this.neighbours.push(grid[this.y - 1][this.x]);
+
+		// RIGHT neighbour
+		if (this.x < this.totalRows - 1 && !grid[this.y][this.x + 1].isBarrier())
+			this.neighbours.push(grid[this.y][this.x + 1]);
+
+		// LEFT neighbour
+		if (this.x > 0 && !grid[this.y][this.x - 1].isBarrier())
+			this.neighbours.push(grid[this.y][this.x - 1]);
+	}
+}
+
+export const getGrid = function (gridSize) {
+	const grid = [];
+	for (let i = 0; i < gridSize; i++) {
+		const row = [];
+
+		for (let j = 0; j < gridSize; j++) row.push(new Cell(j, i, 0, gridSize));
+
+		grid.push(row);
+	}
+
+	return grid;
+};
+
+export const getGridStates = function (grid) {
+	return grid.map(row => row.map(cell => cell.state));
+};
+
+export const cellToCoords = function (cell) {
+	return `${cell.x},${cell.y}`;
+};
+
+export const coordsToCell = function (grid, coordsString) {
+	const [x, y] = coordsString.split(',');
+	return grid[+y][+x];
+};
